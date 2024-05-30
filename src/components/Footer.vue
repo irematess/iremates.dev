a7fda0ad23e8dad70cc3a90c870864b3ebe0f4a4
irem ates <irematees16@gmail.com>
2024-05-30 11:41:32 +0300

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup></script>
+<template>
+  <div
+    class="w-1/2 mx-auto flex justify-center gap-16 px-4 py-3 rounded-lg items-center text-4xl text-primary"
+  >
+    <a href="https://github.com/irematess"><i class="fa-brands fa-github box"></i></a>
+    <a href="https://www.linkedin.com/in/irematees/"><i class="fa-brands fa-linkedin box"></i></a>
+    <a href="https://twitter.com/iremats16"><i class="fa-brands fa-x-twitter box"></i></a>
+    <a href="https://medium.com/@irematees16"><i class="fa-brands fa-medium box"></i></a>
+  </div>
+</template>
--- src/views/layouts/MainLayout.vue
+++ src/views/layouts/MainLayout.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 <template>
   <div class="py-16 flex flex-col gap-16">
     <Header />
-
     <RouterView />
+    <Footer />
   </div>
 </template>
 <style scope></style>
