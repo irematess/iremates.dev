80343d8aa86402bd5615c362fa422273c3a50301
irem ates <irematees16@gmail.com>
2024-06-07 17:47:32 +0300

    Fix width

--- src/views/layouts/MainLayout.vue
+++ src/views/layouts/MainLayout.vue
@@ -3,7 +3,7 @@ import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 </script>
 <template>
-  <div class="w-full py-28 flex flex-col items-center gap-16">
+  <div class="w-full h-full py-28 flex flex-col items-center gap-16">
     <Header />
     <RouterView />
     <Footer />
